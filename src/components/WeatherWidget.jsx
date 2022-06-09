3e344759862a0ff0397d386c5b97b250b2115551
Theodor Will <theodor.will@gmail.com>
2022-06-09 16:25:00 +0200

    Codebase with some basic functionallity

---
+++ src/components/WeatherWidget.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class WeatherWidget extends Component {
+  render() {
+    return (
+      <div>WeatherWidget</div>
+    )
+  }
+}
---
+++ src/views/Home.jsx
@@ -0,0 +1,13 @@
+import useFetch from '../api/useFetch'
+
+const Home = () => {
+
+
+    return (
+        <div className="home">
+            <h1>HELLO I AM HOME! NICE TO MEET YOU! EXDEE</h1>
+        </div>
+    )
+}
+
+export default Home
